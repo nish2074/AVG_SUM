dab4af8dd25f2b9b26c2a53f8fe6fadc2855e4d1
nish2074 <147603920+nish2074@users.noreply.github.com>
2023-10-11 15:23:23 +0530

    Create Basic_avg.cpp

---
+++ Basic_avg.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+float a,b,c,d,e,sum;
+cout<<"enter the 5 numbers:\n";
+    cin>>a>>b>>c>>d>>e;
+    cout<<"sum of 5 numbers is:\t"<<a+b+c+d+e<<endl;
+    sum=a+b+c+d+e;
+    cout<<"avg of 5 numbers is:\t"<<sum/5;
+    
+    return 0;
+}
